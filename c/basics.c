5d77cec6e44553426d9445ed6d5c3cb234530584
Josiah O'Neill <2027oneill.joey@fcboe.org>
2024-01-22 14:49:52 -0500

    c basics

---
+++ c/basics.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+printf("This is my c basics file\n"); 	//write an output statement
+int x; 					// type name
+printf("Enter a number: ");		// promt user for a number
+scanf("%i", &x);   			// store user input in num
+printf("Your number was %i\n", x);       					
+	return 0;
+}	
+
+
+
+
