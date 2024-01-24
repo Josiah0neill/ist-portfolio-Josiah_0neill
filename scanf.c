1a8bb902560ec2dfcd6a09d6c3276a2cb2e7a951
Josiah O'Neill <2027oneill.joey@fcboe.org>
2024-01-24 14:32:55 -0500

    scanf.c assignment

---
+++ scanf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+int age;
+float gpa;
+char grade;
+
+printf("how old are you?");
+scanf("%i", &age);
+
+printf("what is your gpa?");
+scanf("%f", &gpa);
+
+printf("what is your letter grade?");
+scanf(" %c", &grade);
+
+
+printf("your age is %i, your gpa is %f, your grade is %c\n,", age, gpa, grade);
+
+return 0
+}
